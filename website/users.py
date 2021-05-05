6d24d50aaa7bd80684cb84f3402644541a0d0098
Ghazi <ghazi.abukhalaf@gmail.com>
2021-05-05 19:10:51 +0300

    medbay

--- website/users.py
+++ website/users.py
@@ -78,11 +78,6 @@ def about_us_view():
 @login_required
 def profile_view():
     if current_user.is_patient():
-        print(current_user.first_name)
-        print(current_user.last_name)
-        print(current_user.age())
-        print(current_user.phone_no)
-        print(current_user.email)
         information = patient_appointments(current_user.id)  
         return render_template("profile.html",user=current_user, information=information, today=today, sidebar=PATIENT_SIDEBAR)    
     elif current_user.is_medical_staff():
@@ -101,12 +96,18 @@ def profile_view_phone():
     if request.mimetype == 'application/json':
             if load_user_request(request):
                 if current_user.is_patient():
+                    return jsonify({'firstname':current_user.first_name,'lastname':current_user.last_name,'age':current_user.age(),'phone':current_user.phone_no,'email':current_user.email})
+@user_view.route("/appointment_history")
+@login_required
+def appointment_history():
+    day=[],month=[],year=[]firstname=[],lastname=[],hospital=[],department=[],weekday=[],hour=[],minute=[]
+    
+    if request.mimetype == 'application/json':
+            if load_user_request(request):
+                if current_user.is_patient():
                     information = patient_appointments(current_user.id) 
                     for appointment,hospital,department,usr,diagnoses,lab_results in information:
                         if appointment.appointment_date_time < today:
-                            print(usr.first_name)
-                    return jsonify({'firstname':current_user.first_name,'lastname':current_user.last_name,'age':current_user.age(),'phone':current_user.phone_no,'email':current_user.email})
-
 #Edit_Profile View
 @user_view.route("/edit_profile", methods=["POST", "GET"])
 @login_required
