d631b9cf755584fbead2d78730dd5e3436491ba6
Ghazi <ghazi.abukhalaf@gmail.com>
2021-05-10 20:55:56 +0300

    medbay

--- website/users.py
+++ website/users.py
@@ -551,9 +551,9 @@ def appointment_time_select_View():
     data = [{"id": time_slot[0], "start": (time_slot[2].strftime("%H:%M")).__str__()} for time_slot in time_slots]
     return jsonify(data)  
 
-@user_view.route("/appointment_time_select_phone<int:medical_staff_id><string:appointment_date>", methods=["GET"])
+@user_view.route("/appointment_time_select_phone", methods=["GET"])
 @login_required
-def appointment_time_select_View_phone(medical_staff_id,appointment_date):
+def appointment_time_select_View_phone():
     if request.method=="GET":
         if(request.mimetype == 'application/json'):
             if load_user_request(request):
