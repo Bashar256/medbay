229f85cf5f16d3318bd62f29be7a88626e437a04
Ghazi <ghazi.abukhalaf@gmail.com>
2021-05-10 13:30:38 +0300

    medbay

--- website/users.py
+++ website/users.py
@@ -543,6 +543,8 @@ def appointment_time_select_View():
     appointment_times = Appointment_Times.query.filter_by(id=doctor.appointment_times).first()
     appointment_times.create_slots(date=appointment_date)
     time_slots = db.session.query(Time_Slot).filter_by(date=appointment_date.date()).all()
+    print("AVAILABLE TIMES")
+    print(time_slots)
     data = [{"id": time_slot[0], "start": (time_slot[2].strftime("%H:%M")).__str__()} for time_slot in time_slots]
     return jsonify(data)  
 
