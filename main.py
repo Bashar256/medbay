0262e24fc40ff31b2c79aa8dad83dd4e3068aead
Bashar256 <bashar.n.bader@gmail.com>
2021-05-15 15:10:14 +0300

    medbay

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from website import app
 from website.models import Hospital
 from website.temp_create_objects import create_stuff
 
+if not Hospital.query.all():
+    create_stuff()
+
 if __name__ == "__main__":
-    if not Hospital.query.all():
-        create_stuff()
     app.run(debug=True, host="0.0.0.0", port=5000)
